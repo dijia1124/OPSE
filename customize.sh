7d57254bf1c65ed16de95fbff60180657b567bf1
dijia1124 <34638934+dijia1124@users.noreply.github.com>
2020-06-19 00:19:53 +0800

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,5 @@
+ui_print "*******************************************"
+ui_print "Made by xda@dijia1124 (coolapk@你才是大笨猪)"
+ui_print "*******************************************"
+ui_print "***************:)**************************"
+set_perm_recursive $MODPATH/system/etc 0 0 0755 0644 
